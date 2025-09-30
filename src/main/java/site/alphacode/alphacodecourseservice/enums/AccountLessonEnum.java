08533ead7983422ea6a3cbab53c2ac982420838d
mineduck1608 <ducdm160804@gmail.com>
2025-09-30 10:55:17 +0700

    UP

--- src/main/java/site/alphacode/alphacodecourseservice/enums/AccountLessonEnum.java
+++ src/main/java/site/alphacode/alphacodecourseservice/enums/AccountLessonEnum.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AccountLessonEnum {
-    IN_PROGRESS(1, "Đang tiến hành"),
-    COMPLETED(2, "Hoàn thành");
+    IN_PROGRESS(1, "ĐANG TIẾN HÀNH"),
+    COMPLETED(2, "HOÀN THÀNH"),;
     private final int code;
     private final String description;
 
--- src/main/java/site/alphacode/alphacodecourseservice/enums/CourseEnum.java
+++ src/main/java/site/alphacode/alphacodecourseservice/enums/CourseEnum.java
@@ -9,7 +9,10 @@ public enum CourseEnum {
 
     DELETED(0, "ĐÃ XÓA"),
     ACTIVE(1, "ĐANG HOẠT ĐỘNG"),
-    INACTIVE(2, "KHÔNG HOẠT ĐỘNG");
+    INACTIVE(2, "KHÔNG HOẠT ĐỘNG"),
+    BEGINNER(3, "DÀNH CHO NGƯỜI MỚI BẮT ĐẦU"),
+    INTERMEDIATE(4, "TRÌNH ĐỘ TRUNG BÌNH"),
+    ADVANCED(5, "TRÌNH ĐỘ NÂNG CAO");
     private final int code;
     private final String description;
 

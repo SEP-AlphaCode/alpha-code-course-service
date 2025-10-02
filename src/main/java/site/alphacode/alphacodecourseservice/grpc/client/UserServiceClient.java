cb8a1865aa4015bd13834e1817db81a323a4e763
mineduck1608 <ducdm160804@gmail.com>
2025-10-02 14:02:54 +0700

    up

--- src/main/java/site/alphacode/alphacodecourseservice/grpc/client/UserServiceClient.java
+++ src/main/java/site/alphacode/alphacodecourseservice/grpc/client/UserServiceClient.java
@@ -1,42 +1,20 @@
 package site.alphacode.alphacodecourseservice.grpc.client;
-
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 import user.UserServiceGrpc;
 import user.User.AccountInformation;
 import user.User.GetAccountRequest;
 
-import jakarta.annotation.PreDestroy;
-
 @Service
 public class UserServiceClient {
 
-    private final ManagedChannel channel;
-    private final UserServiceGrpc.UserServiceBlockingStub blockingStub;
-
-    public UserServiceClient(
-            @Value("${grpc.user-service.base-url}") String userServiceUrl,
-            @Value("${grpc.user-service.port}") int userServicePort
-    ) {
-        this.channel = ManagedChannelBuilder.forAddress("172.19.0.12", 50051)
-                .usePlaintext() // <-- chạy không TLS
-                .build();
-        this.blockingStub = UserServiceGrpc.newBlockingStub(channel);
-    }
+    @GrpcClient("user-service")
+    private UserServiceGrpc.UserServiceBlockingStub blockingStub;
 
     public AccountInformation getAccount(String accountId) {
         GetAccountRequest request = GetAccountRequest.newBuilder()
                 .setAccountId(accountId)
                 .build();
         return blockingStub.getAccount(request);
-    }
-
-    @PreDestroy
-    public void shutdown() {
-        if (channel != null && !channel.isShutdown()) {
-            channel.shutdown();
-        }
     }
 }

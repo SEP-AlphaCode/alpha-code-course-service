ba29fa75d0c1af03acc9596b1eef5b1a4f9daade
mineduck1608 <ducdm160804@gmail.com>
2025-10-04 20:20:26 +0700

    Create RabbitConfig.java

---
+++ src/main/java/site/alphacode/alphacodecourseservice/config/RabbitConfig.java
@@ -0,0 +1,40 @@
+package site.alphacode.alphacodecourseservice.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public DirectExchange paymentExchange() {
+        return new DirectExchange("payment.exchange");
+    }
+
+    @Bean
+    public Queue courseQueue() {
+        return new Queue("course.create.queue", true);
+    }
+
+    @Bean
+    public Binding bindingCourseQueue(Queue courseQueue, DirectExchange paymentExchange) {
+        return BindingBuilder.bind(courseQueue).to(paymentExchange).with("course.create.queue");
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+}
